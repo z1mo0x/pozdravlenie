95406c235a760e61a128e87c4b63d7f9bcfea0e7
z1mo0x <droweckiy@mail.ru>
2026-09-21 09:34:06 +0300

    final

--- lib/audioManager.ts
+++ lib/audioManager.ts
@@ -15,7 +15,7 @@ export async function playBackgroundMusic(
     if (!backgroundMusic) {
         backgroundMusic = new Audio(src);
         backgroundMusic.loop = true;
-        backgroundMusic.volume = 0.025;
+        backgroundMusic.volume = 0.010;
         backgroundMusic.preload = "auto";
     }
 
